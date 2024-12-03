b62915e25c369543a8654b4a24bba1f1cb534e2b
Antonio Hollerman <toniohollerprof@gmail.com>
2024-12-02 20:03:19 -0500

    create database getters

--- src/main/java/database/SqlManager.java
+++ src/main/java/database/SqlManager.java
@@ -1,13 +1,20 @@
 package database;
 
+import database.records.AccountRow;
+import database.records.RecurringTransactionRow;
+import database.records.TableToRecordAPI;
+import database.records.TransactionRow;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SqlManager {
     private final Connection conn;
-    public SqlManager() {
+    protected SqlManager() {
         try {
             conn = DriverManager.getConnection("jdbc:sqlite:C:\\JavaProjects\\PersonalFinanceManager\\resources\\finance.db");
             createTables();
@@ -46,5 +53,52 @@ public class SqlManager {
             throw new RuntimeException("Failed to create tables");
         }
     }
-    
+    public List<AccountRow> getAccounts(){
+        String sql = """
+                SELECT * FROM account;""";
+        try (PreparedStatement ps = conn.prepareStatement(sql)){
+            return TableToRecordAPI.toAccounts(ps.executeQuery());
+        } catch (SQLException e){
+            e.fillInStackTrace();
+            throw new RuntimeException("Fail to get accounts");
+        }
+    }
+
+    public List<TransactionRow> getTransactions(int[] acc_ids){
+        StringBuilder sql = new StringBuilder("""
+                SELECT * FROM user_transaction WHERE acc_id IN (""");
+        for (int i=0; i < acc_ids.length; i++){
+            if (i != acc_ids.length - 1){
+                sql.append(acc_ids[i]).append(", ");
+            } else {
+                sql.append(acc_ids[i]).append(");");
+            }
+        }
+
+        try (PreparedStatement ps = conn.prepareStatement(sql.toString())){
+            return TableToRecordAPI.toTransactions(ps.executeQuery());
+        } catch (SQLException e){
+            e.fillInStackTrace();
+            throw new RuntimeException("Fail to get transactions");
+        }
+    }
+
+    public List<RecurringTransactionRow> getRecurringTransactions(int[] acc_ids){
+        StringBuilder sql = new StringBuilder("""
+                SELECT * FROM recurring_transaction WHERE acc_id IN (""");
+        for (int i=0; i < acc_ids.length; i++){
+            if (i != acc_ids.length - 1){
+                sql.append(acc_ids[i]).append(", ");
+            } else {
+                sql.append(acc_ids[i]).append(");");
+            }
+        }
+
+        try (PreparedStatement ps = conn.prepareStatement(sql.toString())){
+            return TableToRecordAPI.toRecurringTransactions(ps.executeQuery());
+        } catch (SQLException e){
+            e.fillInStackTrace();
+            throw new RuntimeException("Fail to get transactions");
+        }
+    }
 }
