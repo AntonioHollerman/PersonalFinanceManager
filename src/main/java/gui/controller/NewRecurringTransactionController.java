b53f3810656ba6d5595e45bcc5ab1aa54fe83b76
AntonioHollerman <toniohollerProf@gmail.com>
2024-12-12 14:06:02 -0500

    create structure for new recurring transaction FXML and Controller

--- src/main/java/gui/controller/NewRecurringTransactionController.java
+++ src/main/java/gui/controller/NewRecurringTransactionController.java
@@ -1,4 +1,29 @@
 package gui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class NewRecurringTransactionController {
+    @FXML
+    public ComboBox<String> transactionType;
+    @FXML
+    public ComboBox<String> frequencyType;
+    @FXML
+    public DatePicker dateEntry;
+    @FXML
+    public TextField descEntry;
+    @FXML
+    public TextField amountEntry;
+
+    @FXML
+    public void backButton(){
+
+    }
+
+    @FXML
+    public void saveButton(){
+
+    }
 }
