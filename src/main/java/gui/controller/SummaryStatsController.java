770e930cb2a30d0d4ceaf8f0ce4b0d82bb7304d5
Antonio Hollerman <toniohollerprof@gmail.com>
2024-12-12 01:18:02 -0500

    initialize summary stats FXML and controller structure

--- src/main/java/gui/controller/SummaryStatsController.java
+++ src/main/java/gui/controller/SummaryStatsController.java
@@ -1,4 +1,35 @@
 package gui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+
 public class SummaryStatsController {
+    @FXML
+    public Label income;
+    @FXML
+    public Label expenses;
+
+    @FXML
+    public LineChart balanceToYearLineChart;
+    @FXML
+    public LineChart incomeToYearLineChart;
+    @FXML
+    public LineChart expensesToYearLineChart;
+    @FXML
+    public BarChart expensesToMonthBarChart;
+
+    @FXML
+    public PieChart withdrawsPieGraph;
+    @FXML
+    public PieChart depositsPieGraph;
+    @FXML
+    public PieChart topExpensesPieGraph;
+
+    @FXML
+    public void goBack(){
+
+    }
 }
