f0254eeef34301664be0e993b93f6a1c69c7fec2
Antonio Hollerman <toniohollerprof@gmail.com>
2024-12-11 20:31:51 -0500

    initialize Home FXML and controller structure

--- src/main/java/gui/controller/HomeController.java
+++ src/main/java/gui/controller/HomeController.java
@@ -1,4 +1,35 @@
 package gui.controller;
+// TODO: Preferred height start at 35 for ScrollPane, add 35 for up to 6 rows
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 
 public class HomeController {
+    @FXML
+    private ScrollPane accountsScrollPane;
+
+    @FXML
+    private GridPane accountsGrid;
+
+    @FXML
+    private void initialize() {
+        System.out.println("second");
+    }
+
+    @FXML
+    public void selectButton(ActionEvent e){
+
+    }
+
+    @FXML
+    public void addAccount(){
+
+    }
+
+    @FXML
+    public void openAccounts(){
+
+    }
 }
