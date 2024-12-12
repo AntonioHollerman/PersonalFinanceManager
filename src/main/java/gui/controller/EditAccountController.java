af17dcbe8837c398ed4b5422eae423bcb3eadb9d
Antonio Hollerman <toniohollerprof@gmail.com>
2024-12-12 01:46:46 -0500

    initialize edit account FXML and controller structure

--- src/main/java/gui/controller/EditAccountController.java
+++ src/main/java/gui/controller/EditAccountController.java
@@ -1,8 +1,26 @@
 package gui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import structure.Account;
 
 public class EditAccountController {
+
+    @FXML
+    public TextField nameEntry;
+    @FXML
+    public TextField bankEntry;
+    @FXML
+    public TextField cardEntry;
+
+    @FXML
+    public void backButton(){
+
+    }
+    @FXML
+    public void doneButton(){
+
+    }
     public void renderAccount(Account acc){
 
     }
